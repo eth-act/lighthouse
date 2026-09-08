d81fb5a99554b8d116ab036f294c3d5095590787
frisitano <francesco.risitano95@gmail.com>
2026-09-09 03:53:58 +0800

    Fix sync test proof engine type

--- beacon_node/network/src/sync/tests/mod.rs
+++ beacon_node/network/src/sync/tests/mod.rs
@@ -26,7 +26,13 @@ use types::{ForkName, Hash256, MinimalEthSpec as E, Slot};
 mod lookups;
 mod range;
 
-type T = Witness<ManualSlotClock, E, MemoryStore, MemoryStore>;
+type T = Witness<
+    ManualSlotClock,
+    E,
+    MemoryStore,
+    MemoryStore,
+    <EphemeralHarnessType<E> as beacon_chain::BeaconChainTypes>::ProofEngine,
+>;
 
 /// This test utility enables integration testing of Lighthouse sync components.
 ///
